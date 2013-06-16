e9ae427501413369f66df3f049972a06a7c0e487
zhh <mynamezhh@gmail.com>
2013-06-17 07:47:25 +0800

    socket encapsulation

---
+++ src/core/simp_socket.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (2013) zhh
+ * Copyright (2013) Simple Http, Inc.
+ */
+
+#ifndef CORE_SIMP_SOCKET_H
+#define CORE_SIMP_SOCKET_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <unistd.h>
+
+namespace SIMP_HTTP
+{
+
+class SIMP_Socket
+{
+public:
+	SIMP_Socket() { m_socket = -1; }
+	SIMP_Socket(int fd) : m_socket(fd) {}
+	~SIMP_Socket() {}
+
+public:
+	int Listen(unsigned short port);
+	SIMP_Socket* Accept();
+
+	int Connect(const struct sockaddr* addr);
+
+private:
+	int m_socket;
+};
+
+}
+
+
+#endif // CORE_SIMP_SOCKET_H
